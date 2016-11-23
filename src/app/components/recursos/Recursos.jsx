a504b1d41e197308be30c4912283fec34a1578f9
diegomartin <diegomartinrecillas@outlook.com>
2016-11-23 15:11:46 -0600

    recursos iframe

--- src/app/components/recursos/Recursos.jsx
+++ src/app/components/recursos/Recursos.jsx
@@ -13,28 +13,45 @@ const styles = {
 export default class Recursos extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isLoadingIFrame: true
+        }
     }
     componentDidMount() {
         this.RESOURCES_STORE_ID = ResourcesStore.register(this._onChange);
+        document.getElementById("iframe-recursos").addEventListener('load', this._iFrameFinishedLoading);
     }
 
     componentWillUnmount() {
         ResourcesStore.unregister(this.RESOURCES_STORE_ID);
     }
+
     _onChange = () => {
 
     }
+
+    _iFrameFinishedLoading = () => {
+        this.setState({
+            isLoadingIFrame: false
+        })
+    }
+
     render() {
         let marginTop = -75;
         let width = window.innerWidth;
         let height = window.innerHeight + (-marginTop);
+        let rippleStyle = {transform: 'scale(1)'}
         return (
             <div>
+                <div hidden={!this.state.isLoadingIFrame} className='facebook loader-container'>
+                    <div className='loader uil-ripple-css' style={rippleStyle}><div></div><div></div></div>
+                </div>
                 <iframe
                     style={{
                         width: width,
                         height: height
                     }}
+                    id='iframe-recursos'
                     src='http://comunidadunete.net/buscador/'/>
             </div>
         );

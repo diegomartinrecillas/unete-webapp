dee74f2341bd4f3de7353eb33ee77d838f8047cd
diegomartin <diegomartinrecillas@outlook.com>
2017-01-20 14:20:18 -0600

    release

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -1,15 +1,15 @@
-export const config = {
-    apiKey: "AIzaSyD7s_abdanETtqD0ULOpJJKTF5EoOs3o6c",
-    authDomain: "unete-webapp.firebaseapp.com",
-    databaseURL: "https://unete-webapp.firebaseio.com",
-    storageBucket: "unete-webapp.appspot.com",
-    messagingSenderId: "423792306476"
-};
-
 // export const config = {
-//   apiKey: "AIzaSyA2NbADjpxZ9UZ7EyKVRHD662GTwc-YDK8",
-//   authDomain: "uneteappmaestros.firebaseapp.com",
-//   databaseURL: "https://uneteappmaestros.firebaseio.com",
-//   storageBucket: "uneteappmaestros.appspot.com",
-//   messagingSenderId: "418484459470"
+//     apiKey: "AIzaSyD7s_abdanETtqD0ULOpJJKTF5EoOs3o6c",
+//     authDomain: "unete-webapp.firebaseapp.com",
+//     databaseURL: "https://unete-webapp.firebaseio.com",
+//     storageBucket: "unete-webapp.appspot.com",
+//     messagingSenderId: "423792306476"
 // };
+
+export const config = {
+    apiKey: "AIzaSyAqfjIHqKQydskC_uF3IrIzjsHFi8vwfjM",
+    authDomain: "webapp-maestros.firebaseapp.com",
+    databaseURL: "https://webapp-maestros.firebaseio.com",
+    storageBucket: "webapp-maestros.appspot.com",
+    messagingSenderId: "643627309203"
+};
